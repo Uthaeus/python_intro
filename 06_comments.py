2f3be606bf0b42b33f9a30e5916912b29bdcb968
Uthaeus <romanlavery@gmail.com>
2018-03-23 11:42:40 -0600

    Comments in python

---
+++ 06_comments.py
@@ -0,0 +1,15 @@
+# Three popular ways of adding comments in Python
+
+# Single line:
+# I'm a single line comment
+
+# Inline comment:
+name = 'Kristine' # TODO: Split into two variables
+
+# Multiline comment:
+"""
+aldkfja;ldjfa;dksjfa;dfj
+a;dlksfja;lkdjf;adjfa;dkf
+a;lkdjf;lakdj;fdj;ka;la;j
+a;lsdkjfa;lkdjf;akdjf;adjf
+"""
