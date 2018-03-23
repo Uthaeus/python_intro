b59ddc6b4845cc2d280cd065d1f0fb8dd53f8d65
Uthaeus <romanlavery@gmail.com>
2018-03-23 11:50:00 -0600

    String basics

---
+++ 07_string_basics.py
@@ -0,0 +1,13 @@
+
+sentence = 'The quick brown fox jumped over the lazy dog'
+
+print(sentence)
+# => Single quotes and double quotes are treated the same way in Python
+
+# Escape characters
+sentence_two = 'That is my dog\'s bowl'
+
+# If you know you're going to use apostraphes
+sentence_three = "That is my dog's bowl"
+
+sentence_four = "Tiffany said, \"That is my dog's bowl\""
