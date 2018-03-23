6529c7ee4346708365e1de5eb87a21a65c3f3261
Uthaeus <romanlavery@gmail.com>
2018-03-23 11:25:48 -0600

    Changing variable values

---
+++ 05_change_variable_values.py
@@ -0,0 +1,21 @@
+
+meal_completed = True
+total = 100
+tip = total * 1/5
+total = total + tip
+receipt = "Your total is " + str(total)
+print(receipt) # => Your total is 120.0
+
+
+first = 'Springer'
+second = 'Bregman'
+third = 'Altuve'
+print(second) # => Bregman
+
+second = 'Correa'
+print(second) # => Correa
+
+print(first) # => Springer
+
+first = third
+print(first) # => Altuve
